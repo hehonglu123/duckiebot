7885d3b1f247755b08039175d7f30b9793fc060c
iamnotedible <hehonglu123@126.com>
2019-12-20 15:18:46 -0500

    src

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iamnotedible/duckiebot/catkin_ws/src
-# Build directory: /home/iamnotedible/duckiebot/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("motor_control")
-subdirs("picam")
--- catkin_ws/build/picam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iamnotedible/duckiebot/catkin_ws/src/picam
-# Build directory: /home/iamnotedible/duckiebot/catkin_ws/build/picam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
